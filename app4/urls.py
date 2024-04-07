347b93ad2b3115f284127275a4fccef08883c718
Angelo Laveriano <pechuiyushpluminton@gmail.com>
2024-04-07 06:06:36 -0500

    Subida

---
+++ app4/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name='app4'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('perfilUsuario',views.perfilUsuario,name='perfilUsuario'),
+    path('consolaAdministrador',views.consolaAdministrador,name='consolaAdministrador'),
+    path('cerrarSesion',views.cerrarSesion,name='cerrarSesion'),
+    path('finalizarTarea/<str:idTarea>',views.finalizarTarea,name='finalizarTarea'),
+    path('eliminarTarea/<str:idTarea>',views.eliminarTarea,name='eliminarTarea'),
+    path('reanudarTarea/<str:idTarea>',views.reanudarTarea,name='reanudarTarea'),
+]
